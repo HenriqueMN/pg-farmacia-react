f2fa57e014565354d1998d1fd133609afb529bae
Henrique M. N. <henriquemancininevado@gmail.com>
2024-01-31 16:24:50 -0300

    Implementação das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/footer/Footer"
 import NavBar from "./components/navBar/NavBar"
 import Home from "./pages/home/Home"
+import DeletarCategoria from "./components/categoria/deletarCategoria/DeletarCategoria"
+import FormularioCategoria from "./components/categoria/formularioCategoria/FormularioCategoria"
+import ListarCategorias from "./components/categoria/listarCategoria/ListarCategoria"
 
 
 function App() {
   return (
     <>
-    <NavBar />
-    <Home />
-    <Footer />
+    <BrowserRouter>
+      <NavBar />
+      <div className="min-h-[80vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/categorias" element={<ListarCategorias />} />
+          <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+          <Route path="/editarCategoria/:id" element={<FormularioCategoria />} 
+          />
+          <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} 
+          />  
+        </Routes>
+
+      </div>
+      <Footer />
+    </BrowserRouter>
     </>
   )
 }
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,26 +1,29 @@
-// import {Link, useNavigate} from "react-router-dom"
+import {Link} from "react-router-dom"
 import {ShoppingCart, User} from "@phosphor-icons/react"
 import medicine from "../../assets/medicine.png"
 
 function NavBar() {
-
-    //let navigate = useNavigate()
-
-    //let navbarComponent
-
     return (
         <>
         <div className="w-full bg-slate-800 text-white flex justify-center py-4">
             <div className="container flex justify-between text-lg items-center">
                 <div className="flex gap-4 items-center">
-                <img src={medicine} alt="Desenho de uma mão segurando um comprimido" className="size-12"/>
-                <div className="uppercase text-xl font-semibold">Farmácia Virtual</div>
+                <Link to="/home">
+                    <img src={medicine} alt="Desenho de uma mão segurando um comprimido" className="size-12"/>
+                </Link>
+                <Link to="/home" className="uppercase text-xl font-semibold">
+                    Farmácia Virtual
+                </Link>
                 </div>
                  <input type="text" placeholder="Pesquisar" className="rounded w-2/5 h-fit"/>
                 <div className="flex gap-4">
                     <div className="hover:underline">Produtos</div>
-                    <div className="hover:underline">Categorias</div>
-                    <div className="hover:underline">Cadastrar Categoria</div>
+                    <Link to="/categorias" className="hover:underline">
+                        Categorias
+                    </Link>
+                    <Link to="/cadastroCategoria" className="hover:underline">
+                        Cadastrar Categoria
+                    </Link>
                     <div className="flex gap-2">
                         <User size={32} weight="bold" />
                         <ShoppingCart size={32} weight="bold" />
